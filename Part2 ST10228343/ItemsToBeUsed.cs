a21763b581b93f34521b81d7ca0fd3dc802b2950
Ziyaad Mahomed <108807317+DatTrickster@users.noreply.github.com>
2024-05-30 14:57:24 +0200

    Update ItemsToBeUsed.cs

--- Part2 ST10228343/ItemsToBeUsed.cs
+++ Part2 ST10228343/ItemsToBeUsed.cs
@@ -2,28 +2,14 @@ using System;
 
 namespace Part2_ST10228343
 {
-    // Class representing an item to be used in a recipe
     public class ItemsToBeUsed
     {
-        // Static property to store the number of items
         public static int Length { get; internal set; }
-
-        // Name of the ingredient
         public string NameOfIngrediant { get; set; } = string.Empty;
-
-        // Quantity of the ingredient
         public double Quantity { get; set; }
-
-        // Unit of measurement for the quantity (e.g., grams, cups)
         public string UnitOfmeasurement { get; set; } = string.Empty;
-
-        // Description of the item
         public string DescriptionOfItem { get; set; } = string.Empty;
-
-        // Caloric value of the ingredient
         public double Calories { get; set; }
-
-        // Food group to which the ingredient belongs (e.g., protein, vegetable)
         public string FoodGroup { get; set; } = string.Empty;
     }
 }
